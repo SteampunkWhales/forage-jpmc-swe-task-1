7168f292e621af3e4f74bb2f415857ff1867ded9
SteampunkWhales <100243497+SteampunkWhales@users.noreply.github.com>
2023-12-03 15:34:52 -0500

    completed task 1

--- client_test.py
+++ client_test.py
@@ -1,5 +1,6 @@
 import unittest
 from client3 import getDataPoint
+from client3 import getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,16 +9,26 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    self.assertEqual(getDataPoint(quotes[0]), (quotes[0]['stock'], quotes[0]['top_bid']['price'], quotes[0]['top_ask']['price'], (quotes[0]['top_bid']['price'] + quotes[0]['top_ask']['price']) /2))
+    self.assertEqual(getDataPoint(quotes[0]), (quotes[0]['stock'], quotes[0]['top_bid']['price'], quotes[0]['top_ask']['price'],(quotes[0]['top_bid']['price'] + quotes[0]['top_ask']['price']) / 2))
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    self.assertEqual(getDataPoint(quotes[0]), (quotes[0]['stock'], quotes[0]['top_bid']['price'], quotes[0]['top_ask']['price'],(quotes[0]['top_bid']['price'] + quotes[0]['top_ask']['price']) / 2))
+    self.assertEqual(getDataPoint(quotes[0]), (quotes[0]['stock'], quotes[0]['top_bid']['price'], quotes[0]['top_ask']['price'],(quotes[0]['top_bid']['price'] + quotes[0]['top_ask']['price']) / 2))
 
   """ ------------ Add more unit tests ------------ """
+  def test_getRatio_calculate_ratio(self):
+    self.assertEqual((2/3), getRatio(2, 3))
+
+  def test_getRatio_handles_zero_a(self):
+    self.assertEqual(0, getRatio(0, 1))
+
+  def test_getRatio_handles_zero_b(self):
+    self.assertEqual(0, getRatio(0, 1))
 
 
 
